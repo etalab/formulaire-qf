75422828fc16c6a2613d3135e8214b1e1ede7c0b
Jean-Baptiste Feldis <5403+jbfeldis@users.noreply.github.com>
2024-04-22 09:20:23 +0200

    Ajoute FranceConnect ainsi qu'un squelette de navigation
    Ce commit ajoute une connexion basique à FranceConnect et génère des
    commandes curls pour tester la récupération des données auprès
    d'api-particulier.

---
+++ app/models/pivot_identity.rb
@@ -0,0 +1,27 @@
+class PivotIdentity
+  extend HashMapper
+
+  attr_reader :auth, :data
+
+  map from("info/birthdate"), to("birthdate") { |str| Date.strptime(str, "%Y-%m-%d") }
+  map from("info/family_name"), to("family_name")
+  map from("info/first_name"), to("first_names") { |str| str.split(" ") }
+  map from("info/gender"), to("gender")
+  map from("info/last_name"), to("last_name")
+  map from("extra/raw_info/birthcountry"), to("birth_country")
+  map from("extra/raw_info/birthplace"), to("birthplace")
+
+  map from("credentials/token"), to("token")
+
+  def initialize(recipient:, auth: {})
+    @auth = auth
+    @data = self.class.normalize(auth)
+    @data[:recipient] = recipient
+  end
+
+  delegate :[], to: :data
+
+  def token
+    self[:token]
+  end
+end
