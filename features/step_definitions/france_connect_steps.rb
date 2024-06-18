28483312882c6af79ae62f54b24f07cb83e53a24
Caillou <6117264+JeSuisUnCaillou@users.noreply.github.com>
2024-06-18 12:16:20 +0200

    add webmock in cucumber tests

--- features/step_definitions/france_connect_steps.rb
+++ features/step_definitions/france_connect_steps.rb
@@ -18,6 +18,12 @@ def mock_france_connect
   })
 end
 
+def mock_api_particulier
+  login_uri_template = Addressable::Template.new "https://staging.particulier.api.gouv.fr/api/v2/composition-familiale-v2?recipient={siret}"
+  stub_request(:get, login_uri_template).to_return(body: "{}") # TODO: mock the api_particulier's data
+end
+
 Sachantque("je suis un utilisateur qui peut se france connecter") do
   mock_france_connect
+  mock_api_particulier
 end
