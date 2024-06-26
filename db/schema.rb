d5cefce3ecffbcf6abef5f2f0199815c7287bbad
Jean-Baptiste Feldis <5403+jbfeldis@users.noreply.github.com>
2024-06-26 10:46:38 +0200

    Enlève le sub de la BDD shipments

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_19_135804) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_26_080052) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -112,7 +112,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_19_135804) do
   end
 
   create_table "shipments", force: :cascade do |t|
-    t.string "sub"
     t.string "hubee_folder_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
