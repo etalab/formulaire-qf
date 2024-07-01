050ad587a7b07137ba6e1e1c2419241a305b3861
Caillou <6117264+JeSuisUnCaillou@users.noreply.github.com>
2024-07-01 16:36:49 +0200

    GetFamilyQuotient uses interactor#fail!

--- spec/interactors/get_family_quotient_spec.rb
+++ spec/interactors/get_family_quotient_spec.rb
@@ -16,8 +16,21 @@ describe GetFamilyQuotient, type: :interactor do
       expect { call }.not_to raise_error
     end
 
-    it "sets up the current quotient familial" do
+    it "sets up the quotient familial" do
+      expect(call.success?).to be true
       expect(call.quotient_familial).to match(hash_including("quotientFamilial" => 2550))
+    end
+
+    context "when the API returns an error" do
+      before do
+        stub_qf_v2(kind: :not_found)
+      end
+
+      it "sets up a failure" do
+        expect(call.success?).to be false
+        expect(call.quotient_familial).to be_nil
+        expect(call.message).to eq("Dossier allocataire inexistant. Le document ne peut être édité.")
+      end
     end
   end
 end
