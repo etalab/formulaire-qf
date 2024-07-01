eb7abd59951199f14316e99dd543013cb5677ce3
Caillou <6117264+JeSuisUnCaillou@users.noreply.github.com>
2024-07-01 16:36:49 +0200

    GetFamilyQuotient uses interactor#fail!

--- app/interactors/get_family_quotient.rb
+++ app/interactors/get_family_quotient.rb
@@ -1,5 +1,15 @@
 class GetFamilyQuotient < BaseInteractor
   def call
-    context.quotient_familial = ApiParticulier::QuotientFamilialV2.get(access_token: context.user.access_token, siret: context.siret)
+    quotient_familial = ApiParticulier::QuotientFamilialV2.get(access_token: context.user.access_token, siret: context.siret)
+
+    if quotient_familial["error"].present?
+      context.fail!(message: error_message(quotient_familial))
+    else
+      context.quotient_familial = quotient_familial
+    end
+  end
+
+  def error_message(quotient_familial)
+    quotient_familial["message"] || quotient_familial["reason"] || quotient_familial["error"]
   end
 end
