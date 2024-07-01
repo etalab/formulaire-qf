5b82b8df06040c5c7326b2cb06f8686086de52e6
Caillou <6117264+JeSuisUnCaillou@users.noreply.github.com>
2024-07-01 16:04:15 +0200

    keep "recipient" in the hubee vocabulary, and use "siret" everywhere we can in our codebase

--- app/interactors/get_family_quotient.rb
+++ app/interactors/get_family_quotient.rb
@@ -1,5 +1,5 @@
 class GetFamilyQuotient < BaseInteractor
   def call
-    context.quotient_familial = ApiParticulier::QuotientFamilialV2.get(access_token: context.user.access_token, recipient: context.recipient)
+    context.quotient_familial = ApiParticulier::QuotientFamilialV2.get(access_token: context.user.access_token, siret: context.siret)
   end
 end
