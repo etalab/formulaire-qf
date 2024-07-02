0f5e92e3dfa1ec9a037a346280a4b69405e150cc
Caillou <6117264+JeSuisUnCaillou@users.noreply.github.com>
2024-07-02 15:30:04 +0200

    memorize the @quotient_familial

--- app/interactors/get_family_quotient.rb
+++ app/interactors/get_family_quotient.rb
@@ -1,15 +1,19 @@
 class GetFamilyQuotient < BaseInteractor
   def call
-    quotient_familial = ApiParticulier::QuotientFamilialV2.get(access_token: context.user.access_token, siret: context.siret)
-
     if quotient_familial["error"].present?
-      context.fail!(message: error_message(quotient_familial))
+      context.fail!(message: error_message)
     else
       context.quotient_familial = quotient_familial
     end
   end
 
-  def error_message(quotient_familial)
+  def error_message
     quotient_familial["message"] || quotient_familial["reason"] || quotient_familial["error"]
+  end
+
+  private
+
+  def quotient_familial
+    @quotient_familial ||= ApiParticulier::QuotientFamilialV2.get(access_token: context.user.access_token, siret: context.siret)
   end
 end
