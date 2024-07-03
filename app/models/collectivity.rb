3a49e7576479a24f9175bffdc4eb7acb8de03ec7
Caillou <6117264+JeSuisUnCaillou@users.noreply.github.com>
2024-07-03 16:55:29 +0200

    test the siret validation

--- app/models/collectivity.rb
+++ app/models/collectivity.rb
@@ -1,4 +1,5 @@
 class Collectivity < ApplicationRecord
   enum status: {active: "active", inactive: "inactive"}
   validates :name, :siret, :code_cog, :status, presence: true
+  validates :siret, siret: true
 end
--- spec/models/collectivity_spec.rb
+++ spec/models/collectivity_spec.rb
@@ -5,4 +5,22 @@ RSpec.describe Collectivity, type: :model do
   it { is_expected.to validate_presence_of(:status) }
 
   it { is_expected.to define_enum_for(:status).with_values(active: "active", inactive: "inactive").backed_by_column_of_type(:string) }
+
+  describe "validates the siret" do
+    context "with a valid siret" do
+      let(:collectivity) { build(:collectivity, siret: "13002526500013") }
+
+      it "is valid" do
+        expect(collectivity).to be_valid
+      end
+    end
+
+    context "with an invalid siret" do
+      let(:collectivity) { build(:collectivity, siret: "123") }
+
+      it "is invalid" do
+        expect(collectivity).to be_invalid
+      end
+    end
+  end
 end
