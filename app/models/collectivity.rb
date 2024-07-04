832ff4172ced05f02419e3155ca36743163927fb
Caillou <6117264+JeSuisUnCaillou@users.noreply.github.com>
2024-07-04 15:19:45 +0200

    shipment belongs_to collectivity

--- app/models/collectivity.rb
+++ app/models/collectivity.rb
@@ -2,4 +2,6 @@ class Collectivity < ApplicationRecord
   enum status: {active: "active", inactive: "inactive"}
   validates :name, :siret, :code_cog, :status, presence: true
   validates :siret, siret: true
+
+  has_many :shipments, dependent: :destroy
 end
--- spec/factories/shipments.rb
+++ spec/factories/shipments.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
   factory :shipment do
+    collectivity
+
     hubee_folder_id { "folder_uuid" }
     hubee_status { :pending }
   end
