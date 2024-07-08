b7d7dbdb506a1db716a29742b353bff467a1ee97
Jean-Baptiste Feldis <5403+jbfeldis@users.noreply.github.com>
2024-07-08 10:04:58 +0200

    Ajoute le job à la crontab good_job

--- config/initializers/good_job.rb
+++ config/initializers/good_job.rb
@@ -3,6 +3,7 @@ Rails.application.configure do
   config.good_job.enable_cron = ENV["FRONTAL"] == "true"
   config.good_job.cron = {
     populate_hubee_sandbox: {cron: "0 4 * * *", class: "PopulateHubEESandboxJob"},
+    process_hubee_notifications: {cron: "0 */4 * * *", class: "ReadHubEENotificationsJob"},
   }
 end
 
