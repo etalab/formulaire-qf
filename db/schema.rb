6ce58e4b6f6d96a8d419ad205cbf94dcd535b432
Caillou <6117264+JeSuisUnCaillou@users.noreply.github.com>
2024-07-09 10:22:07 +0200

    add db constraint on shipment.collectivity_id

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_04_131031) do
+ActiveRecord::Schema[7.1].define(version: 2024_07_09_081757) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -117,7 +117,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_04_131031) do
     t.datetime "updated_at", null: false
     t.string "reference", null: false
     t.string "hubee_status", default: "pending"
-    t.bigint "collectivity_id"
+    t.bigint "collectivity_id", null: false
     t.index ["collectivity_id"], name: "index_shipments_on_collectivity_id"
     t.index ["hubee_status"], name: "index_shipments_on_hubee_status"
     t.index ["reference"], name: "index_shipments_on_reference", unique: true
