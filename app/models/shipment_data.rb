d5b2956fb8df27aef3838fb4dbe8a01c01a8d6a6
Caillou <6117264+JeSuisUnCaillou@users.noreply.github.com>
2024-07-18 11:50:35 +0200

    rename qf_error

--- app/models/shipment_data.rb
+++ app/models/shipment_data.rb
@@ -49,7 +49,7 @@ class ShipmentData
     if quotient_familial.blank?
       <<~TEXT
         Quotient familial:
-          ERREUR: #{I18n.t("shipments.error.title")}
+          ERREUR: #{I18n.t("shipments.qf_error.title")}
       TEXT
     else
       <<~TEXT
