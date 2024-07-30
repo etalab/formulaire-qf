2765ef8b2e0009b5608d3ced7bf2f61655479b32
Jean-Baptiste Feldis <5403+jbfeldis@users.noreply.github.com>
2024-07-30 17:26:48 +0200

    Met en place le retry sur le job de lecture de notifications hubee

--- app/jobs/read_hubee_notifications_job.rb
+++ app/jobs/read_hubee_notifications_job.rb
@@ -1,6 +1,8 @@
 class ReadHubEENotificationsJob < ApplicationJob
   queue_as :default
 
+  retry_on Net::ReadTimeout, wait: :polynomially_longer, attempts: 8
+
   def perform(items_count: 100)
     notifications = HubEE::Api.session.notifications(items_count:).body
     notifications.each do |notification|
