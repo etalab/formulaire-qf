7c550b4116f605979b3077f1c152a89f6b271d34
Caillou <6117264+JeSuisUnCaillou@users.noreply.github.com>
2024-08-01 16:45:10 +0200

    request hebee on the 10th min of the hour (#95)

--- config/initializers/good_job.rb
+++ config/initializers/good_job.rb
@@ -2,8 +2,8 @@ Rails.application.configure do
   config.good_job.dashboard_default_locale = :fr
   config.good_job.enable_cron = ENV["FRONTAL"] == "true"
   config.good_job.cron = {
-    populate_hubee_sandbox: {cron: "0 4 * * *", class: "PopulateHubEESandboxJob"},
-    process_hubee_notifications: {cron: "0 */4 * * *", class: "ReadHubEENotificationsJob"},
+    populate_hubee_sandbox: {cron: "10 4 * * *", class: "PopulateHubEESandboxJob"},
+    process_hubee_notifications: {cron: "10 */4 * * *", class: "ReadHubEENotificationsJob"},
   }
 end
 
