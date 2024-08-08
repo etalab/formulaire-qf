2fd6e5de2c8a8a81a8614db45cec97213493c0d8
Caillou <6117264+JeSuisUnCaillou@users.noreply.github.com>
2024-08-08 18:33:17 +0200

    active record session store

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_09_081757) do
+ActiveRecord::Schema[7.1].define(version: 2024_08_08_162842) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -111,14 +111,23 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_09_081757) do
     t.index ["scheduled_at"], name: "index_good_jobs_on_scheduled_at", where: "(finished_at IS NULL)"
   end
 
+  create_table "sessions", force: :cascade do |t|
+    t.string "session_id", null: false
+    t.text "data"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["session_id"], name: "index_sessions_on_session_id", unique: true
+    t.index ["updated_at"], name: "index_sessions_on_updated_at"
+  end
+
   create_table "shipments", force: :cascade do |t|
     t.string "hubee_folder_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "reference", null: false
     t.string "hubee_status", default: "pending"
-    t.string "hubee_case_id"
     t.bigint "collectivity_id", null: false
+    t.string "hubee_case_id"
     t.index ["collectivity_id"], name: "index_shipments_on_collectivity_id"
     t.index ["hubee_case_id"], name: "index_shipments_on_hubee_case_id"
     t.index ["hubee_status"], name: "index_shipments_on_hubee_status"
