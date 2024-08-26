7d658c6633c0d8f90902bfcb6c9ae7cc6d3613a6
Jean-Baptiste Feldis <5403+jbfeldis@users.noreply.github.com>
2024-08-26 08:45:09 +0200

    Apply new rubocop enum rule

--- app/models/collectivity.rb
+++ app/models/collectivity.rb
@@ -1,5 +1,5 @@
 class Collectivity < ApplicationRecord
-  enum status: {active: "active", inactive: "inactive"}
+  enum :status, {active: "active", inactive: "inactive"}
   validates :name, :siret, :code_cog, :status, presence: true
   validates :siret, siret: true
 
