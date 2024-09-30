799702f1979a60661d2ef2c3b094e0dae42467dc
Jean-Baptiste Feldis <5403+jbfeldis@users.noreply.github.com>
2024-09-30 14:51:28 +0200

    Empeche la validation du siret pour les valeurs de test hubee (#133)
    Empeche la validation du siret pour les valeurs de test hubee

--- app/models/collectivity.rb
+++ app/models/collectivity.rb
@@ -1,7 +1,14 @@
 class Collectivity < ApplicationRecord
+  TEST_SIRETS = %w[11111111111111 11111111111112].freeze
+
   enum :status, {active: "active", inactive: "inactive"}
   validates :name, :siret, :code_cog, :status, presence: true
-  validates :siret, siret: true, uniqueness: true
+  validates :siret, uniqueness: true
+  validates :siret, siret: true, unless: -> { test_siret? }
 
   has_many :shipments, dependent: :destroy
+
+  def test_siret?
+    TEST_SIRETS.include?(siret)
+  end
 end
--- spec/models/collectivity_spec.rb
+++ spec/models/collectivity_spec.rb
@@ -10,19 +10,29 @@ RSpec.describe Collectivity, type: :model do
   it { is_expected.to define_enum_for(:status).with_values(active: "active", inactive: "inactive").backed_by_column_of_type(:string) }
 
   describe "validates the siret" do
-    context "with a valid siret" do
-      let(:collectivity) { build(:collectivity, siret: "13002526500013") }
+    context "with a real siret" do
+      context "with a valid siret" do
+        let(:collectivity) { build(:collectivity, siret: "13002526500013") }
 
-      it "is valid" do
-        expect(collectivity).to be_valid
+        it "is valid" do
+          expect(collectivity).to be_valid
+        end
+      end
+
+      context "with an invalid siret" do
+        let(:collectivity) { build(:collectivity, siret: "123") }
+
+        it "is invalid" do
+          expect(collectivity).to be_invalid
+        end
       end
     end
 
-    context "with an invalid siret" do
-      let(:collectivity) { build(:collectivity, siret: "123") }
+    context "with a test siret" do
+      let(:collectivity) { build(:collectivity, siret: "11111111111111") }
 
-      it "is invalid" do
-        expect(collectivity).to be_invalid
+      it "is valid" do
+        expect(collectivity).to be_valid
       end
     end
   end
