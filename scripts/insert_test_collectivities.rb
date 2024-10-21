cb766e72b311084502cbace9f5d7f291112529f2
Caillou <6117264+JeSuisUnCaillou@users.noreply.github.com>
2024-10-21 14:35:20 +0200

    one more test collectivity

--- scripts/insert_test_collectivities.rb
+++ scripts/insert_test_collectivities.rb
@@ -2,9 +2,10 @@ collectivities = [
   {siret: "21040107100019", code_cog: "04107", name: "Majastres", departement: "04", status: :active},
   {siret: "11111111111111", code_cog: "11111", name: "Commune de HubEE - Test", departement: "11", status: :active},
   {siret: "11111111111112", code_cog: "11112", name: "Commune de HubEE - API", departement: "11", status: :active},
+  {siret: "11111111111113", code_cog: "11113", name: "Commune de test de Technocarte", departement: "11", status: :active},
 ]
 
 collectivities.each do |collectivity|
   c = Collectivity.new(**collectivity)
-  c.save
+  c.save(validate: false)
 end
