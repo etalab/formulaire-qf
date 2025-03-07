a7c2660b437be30ba5c8fea065a4f2775caaa7d9
Caillou <6117264+JeSuisUnCaillou@users.noreply.github.com>
2025-03-07 11:09:25 +0100

    add sentry message on auth failure

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -2,5 +2,7 @@ class ErrorsController < ApplicationController
   def auth_failure
     @message = params[:message]
     @strategy = params[:strategy]
+
+    Sentry.capture_message("Authentication failure #{@message}", extra: {params:})
   end
 end
