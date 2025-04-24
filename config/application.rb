140dc3f3ae1748efa5d828d222699c100d6fb141
Delmaire Loïc <loic.delmaire@gmail.com>
2025-04-24 15:44:15 +0200

    Autoload lib

--- config/application.rb
+++ config/application.rb
@@ -28,6 +28,8 @@ module QuotientFamilial
     config.i18n.available_locales = [:fr]
     config.i18n.default_locale = :fr
 
+    config.autoload_paths << Rails.root.join("lib")
+
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
@@ -40,6 +42,5 @@ module QuotientFamilial
     # in config/environments, which are processed later.
     #
     # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
   end
 end
